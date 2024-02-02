7a8ce33952b8e58b1e25355e216849feaf02fed1
Mustafa Sarıtaş <mstfsrts@gmail.com>
2024-02-02 22:31:25 +0100

    Update 2nd_Week_Homework.py

--- 2nd_Week_Homework.py
+++ 2nd_Week_Homework.py
@@ -1,22 +1,3 @@
-### Soru1: Öğrenci Notları İşleme
-""""
-Bir öğrenci notlarını işlemek için bir Python programı yazmanız gerekiyor. Programın aşağıdaki işlevleri yerine getirmesi gerekiyor:
-
-Bir sözlük kullanarak 10 öğrencinin bilgilerini ve notlarını saklayın. Her öğrencinin adı, soyadı ve notları(Vize, Final ve Sozlu notu) olsun. Örneğin:
-
-![resim](https://github.com/werhereitacademy/week_2/assets/141542413/dc91b4b6-a51c-4f5f-80a3-854d505ce48a)
-
-1-Her öğrencinin not ortalamasını hesaplayın ve sözlüğe ekleyin.
-
-2-En yüksek not ortalamasına sahip öğrenciyi bulun ve ekrana yazdırın.
-
-3-Her öğrencinin adını soyadından ayırarak ayrı bir tuple içinde saklayın ve bunları bir listeye ekleyin.
-
-4-Adları alfabetik sıraya göre sıralayın ve sıralanmış listeyi ekrana yazdırın.
-
-5-Not ortalaması 70'in altında olan öğrencileri bir küme (set) içinde saklayın.
-"""
-
 ### Question 1: Student Grades Processing
 """You need to write a Python program to process student grades. The program should perform the following functions:
 
@@ -73,22 +54,6 @@ print("Students with an average grade below 70:", below70Students)
 
 ###
 
-###  Soru 2 : Film Kütüphanesi Yönetim Sistemi Projesi
-
-"""Proje Açıklaması: Bu proje, kullanıcının kendi film koleksiyonunu yönetmesine yardımcı olacak bir uygulama oluşturmayı amaçlar. Kullanıcılar filmleri ekleyebilir, düzenleyebilir, silebilir ve koleksiyonlarını görüntüleyebilir.
-
-Kullanılan Veri Yapıları: Sözlükler (filmleri ve ilgili bilgileri saklamak için), listeler (film koleksiyonunu görüntülemek için)
-
-Temel İşlevler:
-
-1-Kullanıcıdan film adı, yönetmen, yayın yılı ve tür gibi bilgileri alarak bir film verisi oluşturun ve bunu bir sözlükte saklayın.
-
-2-Kullanıcıya bir filmi düzenleme veya silme seçeneği sunun.(Bunun icin filme ait hangi veriyi degistirmek istiyorlarsa ona uygun bir fonksiyon yazilmasi gerekir.)
-
-3-Kullanıcının koleksiyonunu görüntülemesine izin verin. Tüm filmleri listeleyin veya tür veya yayın yılı gibi kriterlere göre filtreleyin.
-
-4-Film verilerini bir dosyada saklayın ve programı başlattığınızda bu veriyi geri yükleyin."""
-
 ### Question 2: Movie Library Management System Project
 """
 Project Description: This project aims to create an application that assists users in managing their own film collection. Users can add, edit, delete films, and view their collections.
@@ -105,6 +70,8 @@ Allow the user to view their collection. List all films or filter based on crite
 
 Store film data in a file and reload this data when the program is started.
 """
+
+import json
 
 filmLibrary = []
 
@@ -152,6 +119,21 @@ def deleteMovie():
 
     print(f"{title} movie not found.")
 
+def saveData():
+    with open("filmLibrary.json", "w") as file:
+        json.dump(filmLibrary, file)
+
+def loadData():
+    try:
+        with open("filmLibrary.json", "r") as file:
+            data = json.load(file)
+            filmLibrary.extend(data)
+    except FileNotFoundError:
+        pass
+
+# Load Data
+loadData()
+
 # User interface
 while True:
     print("\n1. Add Movie")
@@ -164,12 +146,15 @@ while True:
 
     if choice == '1':
         addMovie()
+        saveData()
     elif choice == '2':
         viewCollection()
     elif choice == '3':
         editMovie()
+        saveData()
     elif choice == '4':
         deleteMovie()
+        saveData()
     elif choice == '5':
         break
     else:
@@ -179,31 +164,6 @@ while True:
 
 ###
         
-###  Soru 3 :Müşteri Yönetim Sistemi
-"""
-Proje Açıklaması: Bu proje, müşterilerinizi yönetmek ve temel işlemleri gerçekleştirmek için kullanabileceğiniz bir müşteri yönetim sistemi oluşturmanızı içerir. Bu sistem, müşteri bilgilerini saklama, yeni müşteri ekleyebilme, müşteri bilgilerini güncelleyebilme, müşteri silme ve müşteri listesini görüntüleme gibi temel işlevlere sahip olacaktır. İşte projenin temel adımları:
-
-1-Müşteri bilgilerini saklamak için bir sözlük yapısı kullanın. Her müşteri için bir benzersiz müşteri kimliği (ID) atayın ve müşteri bilgilerini bu kimlikle ilişkilendirin. Her müşteri için ad, soyad, e-posta, telefon gibi bilgileri içeren bir sözlük kullanabilirsiniz.
-
-2-Kullanıcıya aşağıdaki işlemleri seçebileceği bir menü sunun:
-
-* Yeni müşteri eklemek
-* Müşteri bilgilerini güncellemek
-* Müşteri silmek
-* Tüm müşterileri listelemek
-* Çıkış yapmak
-
-3-Kullanıcının seçimine göre ilgili işlemi gerçekleştirin. Örneğin, yeni müşteri eklerken kullanıcıdan gerekli bilgileri alın ve sözlüğe yeni bir müşteri ekleyin.
-
-4-Müşteri bilgilerini güncellerken, müşteri kimliğini kullanarak mevcut bilgileri görüntüleyin ve güncellenmiş bilgileri kaydedin.
-
-5-Müşteri silme işleminde kullanıcıdan müşteri kimliğini alın ve bu müşteriyi sözlükten silin.
-
-6-Tüm müşterileri listeleme işleminde, mevcut müşterilerin listesini görüntüleyin.
-
-7-Kullanıcı çıkış yapana kadar işlemleri tekrarlayın.
-"""
-
 ### Question 3: Customer Management System
 """
 Project Description: This project involves creating a customer management system that allows you to manage your customers and perform basic operations. The system will have fundamental functionalities such as storing customer information, adding new customers, updating customer information, deleting customers, and displaying the list of customers. Here are the basic steps for the project:
